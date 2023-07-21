f049943943bae6a0a7c2bae4891c0af199b46393
Garry3Fingers <www.vlad78@yandex.ru>
2023-07-21 21:44:17 +0300

    Create class GameBoard

---
+++ lib/game_board.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# This class implements the playing field for the game.
+# It has public interfaces #apply_change and #display_board
+class GameBoard
+  attr_accessor :board
+
+  def initialize
+    @board = [
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"],
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"],
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"],
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"],
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"],
+      ["\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609", "\u2609"]
+    ]
+  end
+
+  def apply_change(token, column, row = 5)
+    # column -= 1
+    if board[row][column - 1] != "\u2609"
+      apply_change(token, column, row - 1)
+    else
+      board[row][column - 1] = token
+    end
+  end
+
+  def display_board
+    puts ''
+
+    board.each do |arr|
+      arr.each { |item| print "#{item} " }
+      puts ''
+    end
+
+    puts ''
+  end
+end
