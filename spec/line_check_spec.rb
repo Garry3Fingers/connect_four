6872fd32fb581f2ca0b046fc3c8e3fedb7ee3102
Garry3Fingers <www.vlad78@yandex.ru>
2023-07-26 21:49:36 +0300

    Add tests for diagonal_check

--- spec/line_check_spec.rb
+++ spec/line_check_spec.rb
@@ -6,6 +6,7 @@ describe LineCheck do
   describe '#check' do
     before(:each) do
       @token = 'V'
+      @token2 = 'A'
     end
 
     context 'when there is a vertical line of four identical tokens on the board' do
@@ -39,6 +40,27 @@ describe LineCheck do
 
       it 'returns true' do
         expect(line_check.check(@token)).to eq(true)
+      end
+    end
+
+    context 'when there is a diagonal line of four identical tokens on the board' do
+      board = [
+        %w[0 0 0 A 0 0 0],
+        %w[0 0 0 V A 0 0],
+        %w[0 0 V 0 0 A 0],
+        %w[0 V 0 0 0 0 A],
+        %w[V 0 0 0 0 0 0],
+        %w[0 0 0 0 0 0 0]
+      ]
+
+      subject(:line_check) { described_class.new(board) }
+
+      it 'returns true' do
+        expect(line_check.check(@token)).to eq(true)
+      end
+
+      it 'returns true with diagonal in other direction' do
+        expect(line_check.check(@token2)).to eq(true)
       end
     end
   end
